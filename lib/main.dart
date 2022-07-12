19b46e3d957b413eafe73b2a1e21beb91f805207
LeandroTheDev <106118473+LeandroTheDev@users.noreply.github.com>
2022-07-12 15:40:47 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:projeto/models/transactions.dart';
 
+//Inicialização do Aplicativo
 void main() {
   runApp(Expense());
 }
 
 
+
+//Chamada da Página Inicial para a função Aplicativo
 class Expense extends StatelessWidget {
   @override
   Widget build(BuildContext context){
@@ -14,7 +18,27 @@ class Expense extends StatelessWidget {
   }
 }
 
+//Criação da Página Inicial
 class HomePage extends StatelessWidget {
+
+//Informações do Historico de compras
+  final _transactions = [
+    Transaction(
+      id: "t1", 
+      title: "Te amo lara", 
+      value: 310.80, 
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2", 
+      title: "Pneu Velho", 
+      value: 2000.00, 
+      date: DateTime.now(),
+    ),
+  ];
+
+
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -23,10 +47,13 @@ class HomePage extends StatelessWidget {
       ),
 
 
+      //Criação da coluna
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
+          //Criação dos blocos
+          //Bloco 1
           Container(
             child: Card(
               color: Colors.purple,
@@ -34,10 +61,62 @@ class HomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            color: Colors.purple,
-            child: Text("Lista de Compras"),
-          ),
+
+
+          //Criação do Historico de compras
+          //Bloco 2
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+              child: Row(
+                children: [
+
+                  Container(
+                    //Numeros a esquerda
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 10,
+                    ),
+
+                    decoration: BoxDecoration(
+                      border: Border.all( 
+                        color: Colors.purple,
+                        width: 2,
+                      )
+                    ),
+
+                    //Textos da direita
+                    child: Text(
+                      tr.value.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      )
+                    ),
+                  padding: EdgeInsets.all(10),
+                  ),
+                  Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [                   
+                      Text(tr.title,
+                        style: TextStyle(
+                        color: Colors.grey
+                        ),
+                      ),
+                      Text(tr.date.toString(),
+                        style: TextStyle(
+                        color: Colors.grey,
+                        ),
+                      ),
+                    ],
+
+                  )
+                ],
+              ),
+              );
+            }).toList(),
+          )
         ],
       ),
     );
